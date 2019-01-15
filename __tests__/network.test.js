4d1c251d0ca8248840879de8a0aa493ede75b630
Victor Wang(wang shaojie) <wang_shaojie@icloud.com>
2019-01-15 20:02:23 +0800

    network hooks tests

--- __tests__/network.test.js
+++ __tests__/network.test.js
@@ -1,23 +1,66 @@
 import 'react-testing-library/cleanup-after-each'
 import React from 'react'
 import {
-  render,
+  render, wait, fireEvent,
 } from 'react-testing-library'
 import {
-  useOnlineStatus,
+  useOnlineStatus, useFetch,
 } from '../src/index'
 
 test('useOnlineStatus', () => {
+  jest.useFakeTimers()
   Object.defineProperty(window.navigator, 'onLine', { value: false })
   const map = {}
-  window.addEventListener = jest.fn((event, cb) => {
-    map[event] = cb
+  window.addEventListener = jest.fn((eventType, f) => {
+    map[eventType] = f
   })
-
   const OnlineStatus = () => {
     const { online } = useOnlineStatus()
     return <div>{`${online}`}</div>
   }
   const { container } = render(<OnlineStatus />)
+  jest.runAllTimers()
   expect(container.firstChild.textContent).toBe('false')
+  map.online()
+  expect(container.firstChild.textContent).toBe('true')
+  map.offline()
+  expect(container.firstChild.textContent).toBe('false')
+})
+
+test('useFetch', async () => {
+  jest.useFakeTimers()
+  window.fetch = jest.fn(() => new Promise(resolve => resolve({
+    json: () => ({ name: 'Victor' }),
+  })))
+  const Fetch = () => {
+    const { data, loading, setUrl } = useFetch('/test')
+    return (
+      <div>
+        <button
+          data-testid="button"
+          onClick={() => {
+            setUrl('/test2')
+          }}
+        >
+          search
+        </button>
+        <span data-testid="loading">{String(loading)}</span>
+        <span data-testid="data">{JSON.stringify(data)}</span>
+      </div>
+    )
+  }
+  const { getByTestId, getByText } = render(<Fetch />)
+  expect(getByTestId('loading').textContent).toBe('true')
+  jest.runAllTimers()
+  expect(window.fetch).toBeCalledTimes(1)
+  await wait()
+  expect(getByTestId('loading').textContent).toBe('false')
+  expect(getByTestId('data').textContent).toBe(JSON.stringify({ name: 'Victor' }))
+  fireEvent.click(getByText(/search/))
+  jest.runAllTimers()
+  expect(getByTestId('loading').textContent).toBe('true')
+  expect(window.fetch).toBeCalledTimes(2)
+  await wait()
+  expect(getByTestId('loading').textContent).toBe('false')
+  expect(getByTestId('data').textContent).toBe(JSON.stringify({ name: 'Victor' }))
 })
