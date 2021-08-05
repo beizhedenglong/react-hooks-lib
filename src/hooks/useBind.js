27e55ca249a8327d515356406067105c2b7cf292
Victor <wang_shaojie@icloud.com>
2021-08-05 17:11:22 +0800

    feat(usebind): node need to set map for form ele

--- src/hooks/useBind.js
+++ src/hooks/useBind.js
@@ -1,16 +1,42 @@
 import { useState } from 'react'
-import { isNil } from '../utils'
+import { isNil, get } from '../utils'
+
+const defaultMap = (e) => {
+  const tagName = get(e, ['target', 'tagName'])
+  if (tagName !== undefined) {
+    e.persist()
+  }
+  if (tagName === 'INPUT') {
+    if (e.target.type === 'checkbox' || e.target.type === 'radio') {
+      return e.target.checked
+    }
+    return e.target.value
+  }
+  if (tagName === 'SELECT') {
+    const { target } = e
+    if (target.multiple) {
+      const value = Array.from(e.target.selectedOptions, option => option.value)
+      return value
+    }
+    return e.target.value
+  }
+  if (tagName !== undefined) {
+    return e.target.value
+  }
+
+  return e
+}
 
 const useBind = (initial, options = {
   propName: 'value',
   handlerName: 'onChange',
-  map: x => x,
+  map: defaultMap,
 }) => {
   const [value, set] = useState(initial)
   const {
     propName = 'value',
     handlerName = 'onChange',
-    map = x => x,
+    map = defaultMap,
   } = options
   return {
     value,
@@ -18,7 +44,11 @@ const useBind = (initial, options = {
     reset: () => set(initial),
     bind: {
       [propName]: isNil(value) ? initial : value,
-      [handlerName]: newValue => set(map(newValue)),
+      [handlerName]: (newValue) => {
+        set(
+          map(newValue),
+        )
+      },
     },
   }
 }
--- stories/useBind.stories.js
+++ stories/useBind.stories.js
@@ -4,32 +4,52 @@ const section = name => `State|${name}`
 
 
 storiesOf(section('useBind'), module)
-  .addLiveSource('demo', `
-  const UseBind = () => {
-    const input = useBind(
-      'Victor',
-      { map: e => e.target.value },
-    )
-    const checkbox = useBind(
-      true,
-      { propName: 'checked', map: e => e.target.checked },
-    )
+  .addLiveSource('basic', `
+  const Basic = () => {
+    const input = useBind('Victor')
+    const checkbox = useBind(false, { propName: 'checked' })
+    const select = useBind('grapefruit')
+    const multiSelect = useBind(['grapefruit', 'lime'])
     return (
       <div>
-        <div>
-          Input value:
-          {' '}
-          {input.value}
-          <input type="text" {...input.bind} />
-        </div>
-        <div>
-          Checkbox value:
-          {' '}
-          {(checkbox.value.toString())}
-          <input type="checkbox" {...checkbox.bind} />
+        <div style={{
+          display: 'flex', flexDirection: 'column', height: '200px', justifyContent: 'space-around',
+        }}
+        >
+          <div>
+            Input Value:
+            {input.value}
+            <input type="text" {...input.bind} />
+          </div>
+          <div>
+            Checkbox Value:
+            {\`$\{checkbox.value}\`}
+            <input type="checkbox" {...checkbox.bind} />
+          </div>
+          <div>
+            Select Value:
+            {select.value}
+            <select {...select.bind}>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </div>
+          <div>
+            MultiSelect Value:
+            {multiSelect.value.join(',')}
+            <select multiple {...multiSelect.bind}>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </div>
         </div>
       </div>
     )
   }
-  return <UseBind />
+
+  return <Basic />
  `)
